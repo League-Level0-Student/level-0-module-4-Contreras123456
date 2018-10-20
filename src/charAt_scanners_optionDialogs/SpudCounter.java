36c7f0cfc77369df6b53b33fea94070b011143d5
league <league@wts5.attlocal.net>
2018-10-20 12:55:45 -0700

    Did more coding stuff.

---
+++ src/charAt_scanners_optionDialogs/SpudCounter.java
@@ -0,0 +1,22 @@
+package charAt_scanners_optionDialogs;
+
+public class SpudCounter {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i < 9; i++) {
+		if(i==4) {
+			System.out.println(i);
+			 
+		}
+		else if(i==8) {
+			System.out.print(", more");
+		}
+		else {
+			System.out.print(i+" potato "); 
+		}
+		}
+
+		
+	}
+}
